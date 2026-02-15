220bce67797c24892b39c89449c226a69919892a
gonzalovazquez <gonzalovazquez010@gmail.com>
2026-02-14 19:28:31 -0500

    Initial Commit

---
+++ TabSaver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TabSaver
+//
+//  Created by Gonzalo Vazquez on 2026-02-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TabSaver/TabSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabSaverApp.swift
+//  TabSaver
+//
+//  Created by Gonzalo Vazquez on 2026-02-14.
+//
+
+import SwiftUI
+
+@main
+struct TabSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
